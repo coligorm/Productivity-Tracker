e865c898d8b9cb1cf789ff9e1712b06e843c4474
coligorm <colinrhysgorman@gmail.com>
2025-10-08 17:44:41 +1100

    Fixed dependency issue

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -4,63 +4,63 @@ using backend.Models;
 namespace backend.Data
 {
     public class AppDbContext : DbContext
-
+    {
         // Constructor
-        public AppDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    // expose Tasks table for querying and saving Task entities in dbcontext
-    public Dbset<Task> Tasks { get; set; }
-    
-    protected override void OnConfiguration(DbContextOptionsBuilder optionsBuilder)
-    {
-        // Disable warning for model change
-        optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
-    }
+        // expose Tasks table for querying and saving Task entities in dbcontext
+        public DbSet<Task> Tasks { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        // Configure Task entity
-        modelBuilder.Entity<Task>()
-            .Property(t => t.Name)
-            .IsRequired()
-            .HasMaxLength(100);
+        protected override void OnConfiguration(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Disable warning for model change
+            optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
 
-        modelBuilder.Entity<Task>()
-            .Property(t => t.Category)
-            .IsRequired();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure Task entity
+            modelBuilder.Entity<Task>()
+                .Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(100);
 
-        modelBuilder.Entity<Task>()
-            .Property(t => t.Score)
-            .IsRequired()
-            .HasConversion<int>();
+            modelBuilder.Entity<Task>()
+                .Property(t => t.Category)
+                .IsRequired();
 
-        // Add dummy data for testing purposes
-        modelBuilder.Entity<AppDbContext>().HasData(
-            new Task
-            {
-                Name = "Programming",
-                Category = Category.Mind,
-                Score = Score.High
-            },
-            new Task
-            {
-                Name = "Play bass",
-                Category = Category.Soul,
-                Score = Score.Medium
-            },
-            new Task
-            {
-                Name = "Read",
-                Category = Category.Soul,
-                Score = Score.Low
-            },
-            new Task
-            {
-                Name = "Brazilian Jiu-Jitsu",
-                Category = Category.Body,
-                Score = Score.High
-            }
-        );
+            modelBuilder.Entity<Task>()
+                .Property(t => t.Score)
+                .IsRequired()
+                .HasConversion<int>();
 
+            // Add dummy data for testing purposes
+            modelBuilder.Entity<AppDbContext>().HasData(
+                new Task
+                {
+                    Name = "Programming",
+                    Category = Category.Mind,
+                    Score = Score.High
+                },
+                new Task
+                {
+                    Name = "Play bass",
+                    Category = Category.Soul,
+                    Score = Score.Medium
+                },
+                new Task
+                {
+                    Name = "Read",
+                    Category = Category.Soul,
+                    Score = Score.Low
+                },
+                new Task
+                {
+                    Name = "Brazilian Jiu-Jitsu",
+                    Category = Category.Body,
+                    Score = Score.High
+                }
+            );
+        }
     }
 }
