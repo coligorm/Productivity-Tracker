e09d375600960ffac194a780461512aede24948a
coligorm <colinrhysgorman@gmail.com>
2025-10-18 17:06:58 +1100

    Added `CategoryModel.cs` resolved #26 Updated `TaskModel.cs` to reflect new Category Model

---
+++ backend/Models/CategoryModel.cs
@@ -0,0 +1,17 @@
+namespace backend.Models
+
+    class CategoryModel
+{
+    public int Id { get; set; }
+    public CategoryType Type { get; set; }
+    public string Description { get; set; }
+    public int TotalScore { get; set; }
+    public ICollection<TaskModel> Tasks { get; set; } = new List<TaskModel>();
+}
+
+public enum CategoryType
+{
+    Mind,
+    Body,
+    Soul
+}
--- backend/Models/TaskModel.cs
+++ backend/Models/TaskModel.cs
@@ -4,18 +4,14 @@ namespace backend.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Category Category { get; set; }
-        public Score Score { get; set; }
+        public ScoreLevel Score { get; set; }
+
+        // Foreign keys to help one-to-many relationship for Category-to-Task
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
     }
 
-    public enum Category
-    {
-        Mind,
-        Body,
-        Soul
-    }
-
-    public enum Score
+    public enum ScoreLevel
     {
         Low = 1,
         Medium = 2,
