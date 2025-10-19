f419ebd0bdc0c67927b1c6ac264350f26104f13d
coligorm <colinrhysgorman@gmail.com>
2025-10-19 19:31:39 +1100

    Updated db Migrations to represent new seeded data containing CategoryModel

---
+++ backend/Migrations/20251019080758_UpdateSeedDataForCategoryModel.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedDataForCategoryModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Tasks",
+                newName: "CategoryId");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TotalScore = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "TotalScore", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Mental tasks that exercise your Mind", 0, 0 },
+                    { 2, "Physical tasks that exercise your Body", 0, 1 },
+                    { 3, "Express yourself with tasks that grow your Soul", 0, 2 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CategoryId", "Name", "Score" },
+                values: new object[] { 1, "Complete Sudoku", 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CategoryId", "Name", "Score" },
+                values: new object[] { 1, "Programming", 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Do 10 pushups");
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CategoryId", "Name", "Score" },
+                values: new object[] { 2, "Run", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "CategoryId", "Name", "Score" },
+                values: new object[,]
+                {
+                    { 5, 2, "Brazilian Jiu-Jitsu", 3 },
+                    { 6, 3, "Read", 1 },
+                    { 7, 3, "Play bass", 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CategoryId",
+                table: "Tasks",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Categories_CategoryId",
+                table: "Tasks",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Categories_CategoryId",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_CategoryId",
+                table: "Tasks");
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Tasks",
+                newName: "Category");
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Category", "Name", "Score" },
+                values: new object[] { 0, "Programming", 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Category", "Name", "Score" },
+                values: new object[] { 2, "Play bass", 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Read");
+
+            migrationBuilder.UpdateData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Category", "Name", "Score" },
+                values: new object[] { 1, "Brazilian Jiu-Jitsu", 3 });
+        }
+    }
+}
